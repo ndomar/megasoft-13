3acd6a3172edefd3491fba61c4a17e63d1b63adb
sshihata <samy.shihata@yahoo.com>
2013-02-21 01:47:42 +0200

    create HTML bio

---
+++ C4_Samy_profile.js
@@ -0,0 +1,14 @@
+﻿function startup() {
+    $("form").animate({top:150, left:350, width:1080, height: 600}, 200, startup2);
+	$("form").fadeIn(200);
+}
+function startup2() {
+    $(".name1").animate({ left: 620 }, 500, null);
+    $(".name2").animate({ left: 400 }, 500, startup3);
+   
+}
+
+function startup3() {
+    $("img").fadeIn(500, function () { $("div").fadeIn(500); });
+    
+}
