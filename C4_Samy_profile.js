9b4179526f91aa3765dce41525404b3106767329
sshihata <samy.shihata@yahoo.com>
2013-02-21 03:13:33 +0200

    Updated HTML Bio

--- C4_Samy_profile.js
+++ C4_Samy_profile.js
@@ -1,14 +1,13 @@
 ﻿function startup() {
-    $("form").animate({top:150, left:350, width:1080, height: 600}, 200, startup2);
-	$("form").fadeIn(200);
+    $("form").animate({top:150, left:350, width:1080, height: 600}, 500, startup2);
+	$("form").fadeIn(500);
 }
 function startup2() {
-    $(".name1").animate({ left: 620 }, 500, null);
-    $(".name2").animate({ left: 400 }, 500, startup3);
-   
+    
+    $("img").fadeIn(600, startup3);
 }
 
 function startup3() {
-    $("img").fadeIn(500, function () { $("div").fadeIn(500); });
-    
+    $(".name1").animate({ left: 620 }, 500, null);
+    $(".name2").animate({ left: 400 }, 500, function () { $("ul").fadeIn(500); });
 }
