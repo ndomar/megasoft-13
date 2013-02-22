46943cf9e48ac74eb569a65ac22d2ff18fcdfad6
Malatawy15 <malatawy15@gmail.com>
2013-02-22 22:11:30 +0200

    Profile updated

---
+++ CompanyProfiles/Malatawy/Control.js
@@ -0,0 +1,11 @@
+function im1() {
+    document.getElementById('large').setAttribute('src', 'tree.jpg');
+}
+
+function im2() {
+    document.getElementById('large').setAttribute('src', 'natta.jpg');
+}
+
+function im3(){
+    document.getElementById('large').setAttribute('src', 'skatin.jpg');
+}
