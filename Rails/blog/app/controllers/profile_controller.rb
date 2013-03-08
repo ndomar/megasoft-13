600e99f360ddc53d8d735cf5f46b7d1e606077c6
sshihata <samy.shihata@yahoo.com>
2013-03-08 22:50:10 +0200

    added user access rights

--- Rails/blog/app/controllers/profile_controller.rb
+++ Rails/blog/app/controllers/profile_controller.rb
@@ -2,8 +2,12 @@ class ProfileController < ApplicationController
 	def index
 	end
 	def show
+		if (session[:user_id])
 		@current_user = User.find(session[:user_id])
 		@user_blogs = @current_user.user_blogs
+	else
+		render text:"Error 401: access denied "
+	end
 
 	end
 end
