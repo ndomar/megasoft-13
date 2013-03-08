6dda9b0710950a25f1c6139019ec95dfc2581fdd
oss <oss@oss-HP-Mini-210-4000.(none)>
2013-03-08 19:06:35 +0200

    added view

--- Django/task/AddressBookapp/urls.py
+++ Django/task/AddressBookapp/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, url
 
-from polls import views
+from AddressBookapp import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index')
--- Django/task/AddressBookapp/views.py
+++ Django/task/AddressBookapp/views.py
@@ -1,5 +1,13 @@
 # Create your views here.
 from django.http import HttpResponse
-def hello(request):
-    return HttpResponse("Hello, world!")
+from django.template import Context, loader
 
+from AddressBookapp.models import Contacts
+
+def index(request):
+    contact_list = Contacts.objects.filter(username=request.username)[:]
+    template = loader.get_template('AddressBook/index.html')
+    context= Context({
+        'contact_list': contact_list,
+    })
+    return HttpResponse(template.render(context))
