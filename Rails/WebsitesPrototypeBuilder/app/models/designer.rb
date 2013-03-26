7993b40a3c4d1542d4e405b67923df9c2e031894
ahmadsoliman <ahmadadel92@gmail.com>
2013-03-27 01:26:55 +0200

    Allowed user to sign in with his username and facebook email along with email

--- Rails/WebsitesPrototypeBuilder/app/models/designer.rb
+++ Rails/WebsitesPrototypeBuilder/app/models/designer.rb
@@ -3,7 +3,8 @@ class Designer < ActiveRecord::Base
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable,
+         :validatable, :authentication_keys => [:login]
 
 validates :username, :uniqueness => true, :presence => true
 validates :phone_number, :length => { :minimum => 8, :maximum => 11 } 
@@ -14,8 +15,19 @@ validates :country, :presence => true
 validates_numericality_of :day_dob, :greater_than_or_equal_to => 1 , :less_than_or_equal_to =>  31 
 validates_numericality_of :month_dob,:greater_than_or_equal_to =>  1 , :less_than_or_equal_to =>  12 
 validates_numericality_of :year_dob, :greater_than_or_equal_to =>  1900, :less_than_or_equal_to =>  2012
-
+  
+  #virtual attr accessor to sign in with email, username or facebook email
+  attr_accessor :login
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :username, :phone_number, :country,:day_dob,:month_dob, :year_dob, :credit_card_number, :cvv2,:profession, :gender, :facebook_email, :remember_me
-  # attr_accessible :title, :body
+  attr_accessible :login, :email, :password, :password_confirmation, :username, :phone_number, :country,:day_dob,:month_dob, :year_dob, :credit_card_number, :cvv2,:profession, :gender, :facebook_email, :remember_me
+
+  #Override Devise method to log in with email, username or facebook email
+	def self.find_first_by_auth_conditions(warden_conditions)
+	  conditions = warden_conditions.dup
+	  if login = conditions.delete(:login)
+	    where(conditions).where(["lower(facebook_email) = :value OR lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+	  else
+	    where(conditions).first
+	  end
+	end
 end
