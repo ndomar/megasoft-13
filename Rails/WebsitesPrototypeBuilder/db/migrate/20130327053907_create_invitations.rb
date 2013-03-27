dc80e52141c33b91b8144d11cbfd804efdb2b0cb
sshihata <samy.shihata@yahoo.com>
2013-03-27 16:28:03 +0200

    migration

--- Rails/WebsitesPrototypeBuilder/db/migrate/20130327053907_create_invitations.rb
+++ Rails/WebsitesPrototypeBuilder/db/migrate/20130327053907_create_invitations.rb
@@ -2,7 +2,7 @@ class CreateInvitations < ActiveRecord::Migration
   def change
     create_table :invitations do |t|
       t.integer :status
-      t.number :reviewer_id
+      t.integer :reviewer_id
 
       t.timestamps
     end
