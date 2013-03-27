eaf197b1240d8704dc68d8608bff74b5454c9400
Maged Shalaby <maged918@gmail.com>
2013-03-27 23:55:57 +0200

    Finishing up story

--- Rails/WebsitesPrototypeBuilder/app/controllers/task_controller.rb
+++ Rails/WebsitesPrototypeBuilder/app/controllers/task_controller.rb
@@ -1,21 +1,20 @@
 class TaskController < ApplicationController
+
   def invite
-    #Generate URL requires reviewer id, so Invitation.send_invitation can't be the one returning the reviewer, must be implemented in previous step
-    #REGEX validation in controller for correct email format?
-    #reviewer = Reviewer.find_by_email(params[:email])
-    #if @reviewer == nil
-     # @reviewer = Reviewer.create(:email => params[:email]) #Where will the email validation go? That the email provided is in correct format?
-      #Is it rails standard to have simple validation in front end?
-    #end
-   # @status = Invitation.send_invitation(@reviewer, params[:invitation_message], Url_generator.generate_task_url(params[:id],reviewer.id)) 
-    #generate_url should be a method in a tableless model/class/module most probably --> right now is a module in lib/Url)generator.rb
-    #assuming that now send_invitation returns a status (bit/word) indicating successful invtation or not
+    #Sends an email to a new/already existing reviewer with an invitation link to take the desired task.
+    if(params[:email]!=nil) then
+      @reviewer = Reviewer.find_by_email(params[:email])
+      if @reviewer == nil
+        @reviewer = Reviewer.create(:email => params[:email]) 
+      end
+      Invitation.send_invitation(@reviewer, params[:invitation_message], task_path(:task_id => 1, :reviewer_id => @reviewer.id))
+      params[:invitation_email] = "Email sent to " + @reviewer.email 
+    end
   end
 
   def fill_task
-    
-
-
+    #Maps to the user to be filling a task according to his reviewer id.
+    @reviewer = Reviewer.find_by_id[params[:reviewer_id]]
   end
 
 end
