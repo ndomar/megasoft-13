8a9b6d80652e5bbe80123c91aafc3c79f04f4b34
Ahmed Jihad Matar <aj.matar10@gmail.com>
2013-03-28 14:02:48 +0200

    updated gems

--- Rails/WebsitesPrototypeBuilder/config/initializers/secret_token.rb
+++ Rails/WebsitesPrototypeBuilder/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-WebsitesPrototypeBuilder::Application.config.secret_token = 'eaf011f628e9be0a73fe6f44a734d8daa6158f7930bc6ae5af5f1a1a8c3481822991be11f1f0b2ecd4595e441beb32dd0276efce5feaf810a4eb8b53889ca8d4'
+WebsitesPrototypeBuilder::Application.config.secret_token = 'e499210940d6b136fc6bdfd6ad99c60ff5532810f4532b0c6ac4998db37d61382c6349635c71c0ba6eca3a71ca4fad1e164840b98ccafdd0a3cf0bfc1894b6e1'
