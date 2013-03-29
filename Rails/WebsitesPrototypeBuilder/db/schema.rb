92525324013b6d15ccfdc3e607e141eae45d987b
hoss93 <hoss93@live.com>
2013-03-30 00:52:07 +0200

    more updating with master db

--- Rails/WebsitesPrototypeBuilder/db/schema.rb
+++ Rails/WebsitesPrototypeBuilder/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130329202958) do
+ActiveRecord::Schema.define(:version => 20130329213128) do
 
   create_table "designers", :force => true do |t|
     t.string   "designer_name"
@@ -38,6 +38,13 @@ ActiveRecord::Schema.define(:version => 20130329202958) do
     t.datetime "updated_at",      :null => false
   end
 
+  create_table "pages", :force => true do |t|
+    t.string   "page_name"
+    t.text     "html"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
   create_table "projects", :force => true do |t|
     t.string   "project_name"
     t.string   "type"
@@ -62,6 +69,23 @@ ActiveRecord::Schema.define(:version => 20130329202958) do
     t.string   "email"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+  end
+
+  create_table "task_results", :force => true do |t|
+    t.boolean  "success"
+    t.datetime "time_frame"
+    t.integer  "clicks"
+    t.integer  "task_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "tasks", :force => true do |t|
+    t.string   "name"
+    t.string   "description"
+    t.integer  "project_id"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
   end
 
 end
