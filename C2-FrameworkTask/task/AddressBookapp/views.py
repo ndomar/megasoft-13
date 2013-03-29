843dbe3b7295dbf19737d1c098513c5651da2e78
Sarah Nouh <sarah.nouh17@gmail.com>
2013-03-29 18:27:23 +0200

    remove extra folders

--- C2-FrameworkTask/task/AddressBookapp/views.py
+++
@@ -1,5 +0,0 @@
-# Create your views here.
-from django.http import HttpResponse
-def hello(request):
-    return HttpResponse("Hello, world!")
-
--- Django/task/AddressBookapp/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-class ContactForm(forms.Form):
-    name = forms.CharField()
-    email = forms.EmailField(required=True)
-    number = forms.CharField()
-    address = forms.CharField()
