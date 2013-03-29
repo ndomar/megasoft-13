01ec00fdeec3ad07c4fcefba8ef24d7e3246a82f
hoss93 <hoss93@live.com>
2013-03-29 23:36:58 +0200

    Final one with WebsitesPrototypeBuilder

--- Rails/WebsitesPrototypeBuilder/app/models/project.rb
+++ Rails/WebsitesPrototypeBuilder/app/models/project.rb
@@ -1,3 +1,8 @@
 class Project < ActiveRecord::Base
   attr_accessible :description, :project_name, :type
+
+	def getProjectId
+		self[:id]
+	end
+
 end
--- Rails/WebsitesPrototypeBuilder/db/schema.rb
+++ Rails/WebsitesPrototypeBuilder/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130329202958) do
+ActiveRecord::Schema.define(:version => 20130329213128) do
 
   create_table "designers", :force => true do |t|
     t.string   "designer_name"
@@ -36,6 +36,13 @@ ActiveRecord::Schema.define(:version => 20130329202958) do
     t.integer  "task_id"
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
+  end
+
+  create_table "pages", :force => true do |t|
+    t.string   "page_name"
+    t.text     "html"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "projects", :force => true do |t|
