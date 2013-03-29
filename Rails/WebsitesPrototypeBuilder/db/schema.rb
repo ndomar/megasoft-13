bb935f46b0f76f1fce14d85903251a0fff16bcf5
sshihata <samy.shihata@yahoo.com>
2013-03-29 11:08:54 +0200

    adding UML

--- Rails/WebsitesPrototypeBuilder/db/schema.rb
+++ Rails/WebsitesPrototypeBuilder/db/schema.rb
@@ -11,15 +11,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130327030514) do
+ActiveRecord::Schema.define(:version => 20130327125626) do
 
   create_table "invitations", :force => true do |t|
     t.integer  "status"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.integer  "reviewer_id"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
   end
 
-  create_table "users", :force => true do |t|
+  create_table "reviewers", :force => true do |t|
+    t.string   "email"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
