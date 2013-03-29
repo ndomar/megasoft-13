50a116c16e4b6d0bf097c5d04793ccca6af07edb
hoss93 <hoss93@live.com>
2013-03-30 00:07:01 +0200

    Up to date

--- Rails/WebsitesPrototypeBuilder/db/migrate/20130327005436_add_rpx_identifier_to_designers.rb
+++
@@ -1,8 +0,0 @@
-class AddRpxIdentifierToDesigners < ActiveRecord::Migration
- class Designer < ActiveRecord::Base
- end
-
- def change
-  	add_column :designers, :rpx_identifier, :string
-  end
-end
