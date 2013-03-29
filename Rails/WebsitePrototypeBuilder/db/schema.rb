46ec099c5d7ce17c8ff2d20f33489c24420c6306
hoss93 <hoss93@live.com>
2013-03-29 17:17:18 +0200

    Sprint 0 merged with Database Master

--- Rails/WebsitePrototypeBuilder/db/schema.rb
+++ Rails/WebsitePrototypeBuilder/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130329143933) do
+ActiveRecord::Schema.define(:version => 20130329150708) do
 
   create_table "designers", :force => true do |t|
     t.string   "designer_name"
@@ -35,6 +35,13 @@ ActiveRecord::Schema.define(:version => 20130329143933) do
     t.boolean  "status"
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
+  end
+
+  create_table "pages", :force => true do |t|
+    t.text     "page_name"
+    t.text     "HTML"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "projects", :force => true do |t|
