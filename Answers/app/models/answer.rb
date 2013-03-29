7081e0921dd5147d2aabaf83c6a45ea871318442
Maya Ammar <mayaammar93@gmail.com>
2013-03-29 18:43:10 +0200

    removed code

--- Answers/app/models/answer.rb
+++
@@ -1,6 +0,0 @@
-#Saves answer in the database
-class Answer < ActiveRecord::Base
-  attr_accessible :assigned_part, :body, :page_id, :reviewer
-  belongs_to :page
-  validates :body, :presence => true
-end
