40daf8e935a54b8ab63d0580b2ccc8e7eafdd449
Hesham ElWerdany <blackphoenix.008@gmail.com>
2013-03-29 22:03:42 +0200

    added my migrations

--- Rails/WebsitesPrototypeBuilder/db/migrate/20130327202252_create_tasks.rb
+++ Rails/WebsitesPrototypeBuilder/db/migrate/20130327202252_create_tasks.rb
@@ -1,8 +1,9 @@
 class CreateTasks < ActiveRecord::Migration
   def change
     create_table :tasks do |t|
-      t.string :task_name
-      t.text :description
+      t.string :name
+      t.string :description
+      t.references :project
 
       t.timestamps
     end
