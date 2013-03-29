f135ca49e1959f30e6ff4697eb849fc25cad5e08
hoss93 <hoss93@live.com>
2013-03-29 23:01:23 +0200

    Remove Website

--- Rails/WebsitePrototypeBuilder/app/models/projects.rb
+++
@@ -1,3 +0,0 @@
-class Projects < ActiveRecord::Base
-  attr_accessible :description, :project_name, :type
-end
--- Rails/WebsitePrototypeBuilder/app/models/tasks.rb
+++
@@ -1,3 +0,0 @@
-class Tasks < ActiveRecord::Base
-  attr_accessible :description, :name
-end
