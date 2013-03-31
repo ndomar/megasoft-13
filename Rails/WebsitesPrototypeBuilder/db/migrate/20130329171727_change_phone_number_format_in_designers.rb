fdc1212d94223d01de69111754a2a33dd510a7f0
Ahmed Jihad Matar <aj.matar10@gmail.com>
2013-03-31 03:14:57 +0200

    pulled latest master

--- Rails/WebsitesPrototypeBuilder/db/migrate/20130329171727_change_phone_number_format_in_designers.rb
+++
@@ -1,9 +0,0 @@
-class ChangePhoneNumberFormatInDesigners < ActiveRecord::Migration
-  def self.up
-   change_column :designers, :phone_number, :string
-  end
-
-  def self.down
-   change_column :designers, :phone_number, :int
-  end
-end
