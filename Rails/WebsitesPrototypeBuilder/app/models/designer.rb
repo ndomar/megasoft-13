ec51d5c7e84afd1b4ff4efb6cee6d28983036a13
ahmadsoliman <ahmadadel92@gmail.com>
2013-04-02 19:39:27 +0200

    Wrong syntax resolved on designer model

--- Rails/WebsitesPrototypeBuilder/app/models/designer.rb
+++ Rails/WebsitesPrototypeBuilder/app/models/designer.rb
@@ -9,7 +9,7 @@ class Designer < ActiveRecord::Base
          :validatable, :authentication_keys => [:login]
 
 #Registration attributes and validations
-validates :designer_name, :presence => trues
+validates :designer_name, :presence => true
 validates :phone_number, :length => { :minimum => 8, :maximum => 11 } 
 validates :phone_number,:day_dob,:month_dob,:year_dob, :credit_card_number, :cvv2, :numericality => { :only_integer => true }
 validates :credit_card_number, :length => { :minimum => 12, :maximum => 19 }
