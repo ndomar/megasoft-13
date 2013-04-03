c20549314e26bd469f4dd535e90f13d7c50c05e2
Ahmed Jihad Matar <aj.matar10@gmail.com>
2013-04-04 00:08:47 +0200

    small fix in doc

--- Rails/WebsitesPrototypeBuilder/app/models/answer.rb
+++ Rails/WebsitesPrototypeBuilder/app/models/answer.rb
@@ -1,11 +1,11 @@
 class Answer < ActiveRecord::Base
-	##
+##
 # the answer a user is entering
-# * *Attribute* :
-# - +answer+ -> the reviewer entered his string of answers to be saved
-#* *Relations* :
-# - has foreign key to the page and the question
-# related to one page and one question
+# * *Attribute*    :
+#   - +answer+ -> the reviewer entered his string of answers to be saved
+# * *Relations* :
+#   - has foreign key to the page and the question
+#   - related to one page and one question
   validates :answer,:presence => true
   belongs_to :question
   belongs_to :page
