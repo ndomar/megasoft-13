eda49f3cd1e4ead663f63e5732e33f71ee55ce1f
Menna Ashraf Essawy <mennaessawy12@gmail.com>
2013-04-03 13:44:03 +0200

    create new project (changed documentaion)

--- Rails/WebsitesPrototypeBuilder/app/controllers/projects_controller.rb
+++ Rails/WebsitesPrototypeBuilder/app/controllers/projects_controller.rb
@@ -1,20 +1,25 @@
 class ProjectsController < ApplicationController
-  #to make sure that the designer is logged in
+  #To make sure that the designer is logged in
   before_filter :authenticate_designer! 
-  def new()
+
+    ##
     #Create a new project
+
+  def new()
     @project = Project.new() 
   end
 
+    ##
+    #Create a new project with the info passed by the user (peoject_name : string , Project_type :String, Description : string)
+    #then save(), which will return either true ro false
+    #so if it returned true, go to the project design page
+    #else if it returned false, the project description page will appear again
+
   def create()
-    #Create a new project with the info passed by the user
     @project = Project.new(params[:project])
-    #save, which will return either true ro false
     if @project.save() 
-       #so if it returned true, go to the project design page
       redirect_to @project
     else
-      #else if it returned false, the project description page will appear again
       render"new" 
     end
   end
