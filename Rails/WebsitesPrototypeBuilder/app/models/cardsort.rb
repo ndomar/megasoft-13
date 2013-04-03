59fbae9e7c994fa8702ae65fa2b4f5d8c8d41faa
sshihata <samy.shihata@yahoo.com>
2013-04-03 15:59:14 +0200

    documented

--- Rails/WebsitesPrototypeBuilder/app/models/cardsort.rb
+++ Rails/WebsitesPrototypeBuilder/app/models/cardsort.rb
@@ -1,3 +1,13 @@
+##
+# a brief description of the model
+# **Attribute* :
+# - +description+ -> a short desciption of the card sort and its purpose
+# - +open+ -> boolean to set the type of the cardsort
+# - +project_id+ -> the project the cardsort belongs to
+# - +title+ -> the name of the cardsort
+# **Relations* :
+# - belongs to project
+#
 class Cardsort < ActiveRecord::Base
   attr_accessible :description, :open, :project_id, :title
 end
