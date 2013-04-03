246f8b9d4aed6bb1b49467c2329b019fb547d85f
Sarah Nouh <sarah.nouh17@gmail.com>
2013-04-03 23:52:21 +0200

    A button highlights the current step's component

--- Rails/WebsitesPrototypeBuilder/config/routes.rb
+++ Rails/WebsitesPrototypeBuilder/config/routes.rb
@@ -1,4 +1,7 @@
 WebsitesPrototypeBuilder::Application.routes.draw do
+  resources :tasks do
+    resources :steps
+  end
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +51,7 @@ WebsitesPrototypeBuilder::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'tasks#task_reviewer'
 
   # See how all your routes lay out with "rake routes"
 
