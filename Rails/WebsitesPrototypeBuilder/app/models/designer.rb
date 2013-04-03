e96b296bf9779da35ae567bad70a1b332de45473
ahmadsoliman <ahmadadel92@gmail.com>
2013-04-04 00:53:34 +0200

    Code reviewed

--- Rails/WebsitesPrototypeBuilder/app/models/designer.rb
+++ Rails/WebsitesPrototypeBuilder/app/models/designer.rb
@@ -28,16 +28,17 @@ class Designer < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :rpx_connectable,
          :validatable
 
-#Registration attributes and validations
-validates :fullname, :presence => true
-validates :phone_number, :length => { :minimum => 8, :maximum => 11 } 
-validates :day_dob,:month_dob,:year_dob, :credit_card_number, :cvv2, :numericality => { :only_integer => true }
-validates :credit_card_number, :length => { :minimum => 12, :maximum => 19 }
-validates :cvv2, :length => { :minimum => 3, :maximum => 4 }
-validates :country, :presence => true
-validates_numericality_of :day_dob, :greater_than_or_equal_to => 1 , :less_than_or_equal_to =>  31 
-validates_numericality_of :month_dob,:greater_than_or_equal_to =>  1 , :less_than_or_equal_to =>  12 
-validates_numericality_of :year_dob, :greater_than_or_equal_to =>  1900, :less_than_or_equal_to =>  2012
+  #Registration attributes and validations
+  validates :fullname, :presence => true
+  validates :phone_number, :length => { :minimum => 8, :maximum => 11 } 
+  validates :day_dob,:month_dob,:year_dob, :credit_card_number, :cvv2, :numericality => { :only_integer => true }
+  validates :credit_card_number, :length => { :minimum => 12, :maximum => 19 }
+  validates :cvv2, :length => { :minimum => 3, :maximum => 4 }
+  validates :country, :presence => true
+  validates_numericality_of :day_dob, :greater_than_or_equal_to => 1 , :less_than_or_equal_to =>  31 
+  validates_numericality_of :month_dob,:greater_than_or_equal_to =>  1 , :less_than_or_equal_to =>  12 
+  validates_numericality_of :year_dob, :greater_than_or_equal_to =>  1900, :less_than_or_equal_to =>  2012
+
   # Setup accessible (or protected) attributes for your model
 
   # attr_accessible :title, :body
