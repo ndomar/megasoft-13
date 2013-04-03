12dbd114ac18745c822f17965f866837abe4615e
oss <oss@oss-HP-Mini-210-4000.(none)>
2013-04-03 21:23:53 +0200

    fixed issues raised by code reviewer

--- Rails/WebsitesPrototypeBuilder/app/models/task.rb
+++ Rails/WebsitesPrototypeBuilder/app/models/task.rb
@@ -1,11 +1,11 @@
-class Task < ActiveRecord::Base
+class Task<ActiveRecord::Base
   attr_accessible :description, :name
 
   belongs_to :project
   
-  has_many :task_results
-
   belongs_to :page
+
+  has_many :task_results
 
   validates :name, :presence => true
 end
