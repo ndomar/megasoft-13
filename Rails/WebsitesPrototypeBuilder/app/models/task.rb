58c1cf1973b08101cf9793a4de3b8ec414b982de
karim <kivoktm@gmail.com>
2013-04-04 01:58:09 +0200

    Updated To Master

--- Rails/WebsitesPrototypeBuilder/app/models/task.rb
+++ Rails/WebsitesPrototypeBuilder/app/models/task.rb
@@ -1,7 +1,10 @@
-class Task < ActiveRecord::Base
+class Task<ActiveRecord::Base
   attr_accessible :description, :name
 
   belongs_to :project
+  
+  belongs_to :page
+
+  has_many :task_results
 
   validates :name, :presence => true
-end
--- Rails/WebsitesPrototypeBuilder/config/routes.rb
+++ Rails/WebsitesPrototypeBuilder/config/routes.rb
@@ -6,6 +6,11 @@ WebsitesPrototypeBuilder::Application.routes.draw do
     resources :comments
     resources :questions
   end
+  
+  resources :tasks do
+    resources :task_results
+  end
+  get "/log/:id" => 'task_results#index'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -55,11 +60,10 @@ WebsitesPrototypeBuilder::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'tasks#index'
 
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
-end
