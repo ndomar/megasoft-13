31bdab0b15749d4692a829aa4c5369666dc62287
karim <kivoktm@gmail.com>
2013-04-04 00:18:38 +0200

    Final Documentation

--- Rails/WebsitesPrototypeBuilder/app/controllers/comments_controller.rb
+++ Rails/WebsitesPrototypeBuilder/app/controllers/comments_controller.rb
@@ -1,8 +1,15 @@
 class CommentsController < ApplicationController
-  # called to create a new item in the database
+  # called to create a new comment in the database
+  # finds the selected page
+  # * *Args* :
+  # - +@page+ -> the current page
+  # - +@comment+ -> the comment written in the notepaper
+  # * *Returns* :
+  # - page refreshed and comment added to db
+  #
   def create
     # finds the selected page
-    @page = Page.find(params[:page_id])
+  	@page = Page.find(params[:page_id])
     @comment = @page.comments.build(params[:comment])
     respond_to do |format|
     if @comment.save
@@ -15,7 +22,13 @@ class CommentsController < ApplicationController
     end
   end
 
-# called to delete an item from the database
+  # called to delete a comment from the database
+  # * *Args* :
+  # - +@page+ -> the current page
+  # - +@comment+ -> the comment selected to be deleted
+  # * *Returns* :
+  # - AJAX used to delete answer (slides up and the comment is removed from the db)
+  #
   def destroy
     # find the item
     @comment = Comment.find(params[:id])
@@ -24,7 +37,7 @@ class CommentsController < ApplicationController
     respond_to do |format|
       format.html { redirect_to :controller => :pages, :action => :reviewer, :id => @page ,:notice => 'Comment was successfully deleted.' }
       # Use AJAX for deletion
-      format.js { render :layout => false }
+      format.js   { render :layout => false }
     end
   end
 end
--- Rails/WebsitesPrototypeBuilder/app/controllers/pages_controller.rb
+++ Rails/WebsitesPrototypeBuilder/app/controllers/pages_controller.rb
@@ -1,35 +1,46 @@
 class PagesController < ApplicationController
-
+  # GET /pages
+  # GET /pages.json
   def index
     @pages = Page.all
+
     respond_to do |format|
-      format.html
+      format.html # index.html.erb
       format.json { render json: @pages }
     end
   end
 
+  # GET /pages/1
+  # GET /pages/1.json
   def show
     @page = Page.find(params[:id])  
     respond_to do |format|
-      format.html
+      format.html # show.html.erb
       format.json { render json: @page }
     end
   end
 
+  # GET /pages/new
+  # GET /pages/new.json
   def new
     @page = Page.new
+
     respond_to do |format|
-      format.html
+      format.html # new.html.erb
       format.json { render json: @page }
     end
   end
 
+  # GET /pages/1/edit
   def edit
     @page = Page.find(params[:id])
   end
 
+  # POST /pages
+  # POST /pages.json
   def create
     @page = Page.new(params[:page])
+
     respond_to do |format|
       if @page.save
         format.html { redirect_to @page, notice: 'Page was successfully created.' }
@@ -41,8 +52,11 @@ class PagesController < ApplicationController
     end
   end
 
+  # PUT /pages/1
+  # PUT /pages/1.json
   def update
     @page = Page.find(params[:id])
+
     respond_to do |format|
       if @page.update_attributes(params[:page])
         format.html { redirect_to @page, notice: 'Page was successfully updated.' }
@@ -54,9 +68,12 @@ class PagesController < ApplicationController
     end
   end
 
+  # DELETE /pages/1
+  # DELETE /pages/1.json
   def destroy
     @page = Page.find(params[:id])
     @page.destroy
+
     respond_to do |format|
       format.html { redirect_to pages_url }
       format.json { head :no_content }
