b90cae94ca2c05765c97892a1e4ddc896edebbc2
Menna Ashraf Essawy <mennaessawy12@gmail.com>
2013-04-03 13:46:51 +0200

    create new project (changed documentaion)

--- Rails/WebsitesPrototypeBuilder/app/models/project.rb
+++ Rails/WebsitesPrototypeBuilder/app/models/project.rb
@@ -1,5 +1,17 @@
 # encoding: utf-8
 class Project < ActiveRecord::Base
+
+  ##
+  # Attributes:
+  #  description -> This is the description of the project
+  #  Project_name -> This is the name of the project
+  #  type - > This is the type of the project
+  # Relations :
+  #  A project belongs to one designer
+  # Validations:
+  #  Project name cannot be blank
+  #  Project name should be unique
+  
   belongs_to :designer
   attr_accessible :description, :project_name, :project_type
 
