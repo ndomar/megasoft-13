8f8363a48780558492bd158ea1bebf6c1687984e
Ahmed Jihad Matar <aj.matar10@gmail.com>
2013-04-04 16:33:17 +0200

    answer table

--- Rails/WebsitesPrototypeBuilder/app/models/reviewer.rb
+++ Rails/WebsitesPrototypeBuilder/app/models/reviewer.rb
@@ -1,3 +1,6 @@
 class Reviewer < ActiveRecord::Base
+  
+  has_many :task_results
+  
   attr_accessible :email
 end
---
+++ Rails/WebsitesPrototypeBuilder/app/models/task.rb
@@ -0,0 +1,11 @@
+class Task<ActiveRecord::Base
+  attr_accessible :description, :name
+
+  belongs_to :project
+  
+  belongs_to :page
+
+  has_many :task_results
+
+  validates :name, :presence => true
+end
