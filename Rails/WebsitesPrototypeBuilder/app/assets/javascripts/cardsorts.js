9c6595989318bb3ada32b981d9deb2613cb40479
sshihata <samy.shihata@yahoo.com>
2013-04-05 00:21:25 +0200

    giving user controll on selected card

--- Rails/WebsitesPrototypeBuilder/app/assets/javascripts/cardsorts.js
+++ Rails/WebsitesPrototypeBuilder/app/assets/javascripts/cardsorts.js
@@ -51,3 +51,42 @@ function setTableEffect(){
 		}
 	});
 }
+
+function setCardEffects(){
+	$(".card").click(function (){
+		$(".card").removeClass('selected-card');
+		$(this).addClass('selected-card');
+	});
+
+	$(document).keypress(changeSelected);
+}
+
+function changeSelected(event){
+	var mainCard = $("#main-card").stop();
+
+	var selectedIndex = parseInt($(".selected-card").attr('card-index'));
+	switch(event.keyCode){
+		case 37: 
+			selectedIndex--;
+			break;
+		case 38:
+			selectedIndex += 14;
+			break;
+		case 39:
+			selectedIndex++;
+			break;
+		case 40:
+			selectedIndex -= 14;
+			break;
+		default:
+			return;
+	}
+	var nextSelected = $(".card[card-index="+selectedIndex+"]");
+	if (nextSelected.length){
+		$(".selected-card").removeClass("selected-card");
+		$(nextSelected).addClass("selected-card");
+
+		$(mainCard).fadeOut(300);
+		$(mainCard).fadeIn(300);
+	}
+}
