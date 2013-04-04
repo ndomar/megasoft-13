d93f3e21a4ff6668c6892b239cadecc6b731a983
Marwan El Derwy <marwanelderwy@gmail.com>
2013-04-05 01:14:03 +0200

    typo corrected

--- Rails/WebsitesPrototypeBuilder/app/assets/javascripts/reviewing.js
+++ Rails/WebsitesPrototypeBuilder/app/assets/javascripts/reviewing.js
@@ -83,7 +83,7 @@ $(document).ready(function() {
 			scroll: false
 		});
 });
-//opens the slected page in a new window to test when the test button is clicked
+//called when test page button is clicked in the reviewer's page & opens the slected page in a new window to test when the test button is clicked
 function test_page(){
 	var designed_html= $('#html_content').text();
 	var w = window.open("",null,"height=500,width=1000,status=yes,toolbar=no,menubar=no,location=no");
