2c3b5db4e197a72c9802d2f53f02372767c09da2
hoss93 <hoss93@live.com>
2013-04-06 01:08:20 +0200

    Trying to finish things

--- Rails/WebsitesPrototypeBuilder/app/assets/javascripts/explorer.js
+++ Rails/WebsitesPrototypeBuilder/app/assets/javascripts/explorer.js
@@ -21,5 +21,23 @@ function adding()
 	var t=document.createTextNode("CLICK ME");
 	frame.appendChild(t); 
 	document.body.appendChild(frame);
-	
+	changing();
 }
+
+function changing(){
+
+}
+
+function addPage(){
+	document.write("entered");
+	var input =window.prompt("Please enter Page Name","");
+	//if(input != null){
+		// <%= p = Page.new() %>		
+		// <%= p.page_name = @param %>
+		// <%= p.project_id = @id %>
+		// <%= p.save() %>
+	//}
+}
+
+// <script type="text/javascript">var x = window.prompt("Please enter Page Name","");
+// 	   	 document.write(x);</script>
