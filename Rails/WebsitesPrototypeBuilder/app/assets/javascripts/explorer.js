6a4fa5e7cd4fccd0822a6d5f9d5d5e209128b309
hoss93 <hoss93@live.com>
2013-04-07 01:03:58 +0200

    Firing the function still not done

--- Rails/WebsitesPrototypeBuilder/app/assets/javascripts/explorer.js
+++ Rails/WebsitesPrototypeBuilder/app/assets/javascripts/explorer.js
@@ -1,36 +1,41 @@
-document.write("aaa");
-
-//this is working efficently
-// function adding()
-// {
-	
+//function to be used when i need to add an item to any item
+// function adding(){	
 // 	var div = document.getElementById("div");
 //   var btn=document.createElement("BUTTON");
 // 	var t=document.createTextNode("CLICK ME");
 // 	btn.appendChild(t); 
 // 	document.body.appendChild(btn);
-	
 // }
 
-
-function addingIFrame()
-{
-	document.write("ADDING IFRAME");
-	//var div = document.getElementById("div");
-  var frame=document.createElement("div");
-  var doc = frame.contentDocument;
-  doc.open();
-  doc.write("<p>dsasda</p>");
-  doc.close();
-	//var t=document.createTextNode("CLICK ME");
-	//frame.appendChild(t); 
+//function to create iframes with their attributes
+function createIframeWithContent(id,html){
+	document.write("Step -1");
+	var frame=document.createElement("iframe");
+	frame.setAttribute("id", id); 
 	document.body.appendChild(frame);
-	// changing();
+	document.write("Step 0");
+ 	//the second part is done separetly to make sure that iframe was created
+  frame = document.getElementById(id);													//this is the iframe with the provided id
+	var doc=(frame.contentWindow || frame.contentDocument);				//this is to make it compatible with different browsers
+	if (doc.document)doc=doc.document;														//this is to make it compatible with different browsers
+	document.write("Step 1");
+	doc.open();
+	doc.write(html);
+	doc.close();
+	document.write("Step 2");
+	var script = this.contentWindow.document.createElement("script");
+	script.type = "text/javascript";
+	script.text  = 'if (document.all || document.getElementById) {for (i = 0; i < theform.length; i++) {var formElement = theform.elements[i];if (true) {formElement.disabled = true;}}}';
+	// script to deactivate all actions
+	this.contentWindow.document.body.appendChild(script);
+
+	document.write("FINISHED CREATION");
 }
 
+//function to alter the iframe with given id and fill it with given html
 function addHtml(id,html){
-	var frame = document.getElementById(id);								//this is the iframe with the provided id
-	var doc=(frame.contentWindow || frame.contentDocument);		//this is to make it compatible with different browsers
+	var frame = document.getElementById(id);											//this is the iframe with the provided id
+	var doc=(frame.contentWindow || frame.contentDocument);				//this is to make it compatible with different browsers
 	if (doc.document)doc=doc.document;														//this is to make it compatible with different browsers
 	doc.open();
 	doc.write(html);
@@ -42,32 +47,7 @@ function addHtml(id,html){
 	this.contentWindow.document.body.appendChild(script);
 }
 
-// function addHtml(id,html){
-// 	document.write("1");
-// 	var doc = document.getElementById(id);
-// 	 //var doc = $(this).contentWindow.document;
-// 	// document.write("2");
-// 	// doc.open();
-// 	// document.write("3");
-// 	// doc.write(html);
-// 	// document.write("4");
-// 	// doc.close();
-// 	// document.write("5");
-// 	// var script = this.contentWindow.document.createElement("script");
-// 	// document.write("6");
-// 	// script.type = "text/javascript";
-// 	// document.write("7");
-// 	// script.text  = 'if (document.all || document.getElementById) {for (i = 0; i < theform.length; i++) {var formElement = theform.elements[i];if (true) {formElement.disabled = true;}}}';
-// 	// document.write("8");
-// 	// // script to deactivate all actions
-// 	// this.contentWindow.document.body.appendChild(script);
-// 	document.write("9");
-// }
-
-function changing(){
-
-}
-
+//function used to retrieve name of new page to be created
 function addPage(){
 	document.write("entered");
 	var input =window.prompt("Please enter Page Name","");
@@ -79,5 +59,3 @@ function addPage(){
 	//}
 }
 
-// <script type="text/javascript">var x = window.prompt("Please enter Page Name","");
-// 	   	 document.write(x);</script>
