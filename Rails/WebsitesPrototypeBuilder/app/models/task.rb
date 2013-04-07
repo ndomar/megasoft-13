60ae0f2da8895240a34284695d14d989d0ec8048
sshihata <samy.shihata@yahoo.com>
2013-04-07 18:26:25 +0200

    starting work

--- Rails/WebsitesPrototypeBuilder/app/models/task.rb
+++ Rails/WebsitesPrototypeBuilder/app/models/task.rb
@@ -8,4 +8,24 @@ class Task<ActiveRecord::Base
   has_many :task_results
 
   validates :name, :presence => true
+
+  def invite
+    #Sends an email to a new/already existing reviewer with an invitation link to take the desired task.
+    
+  end
+
+  def self.send_invitation(email, msg, url)
+      @reviewer = Reviewer.find_by_email(email)
+      if @reviewer == nil
+        @reviewer = Reviewer.create(:email => email) 
+      end
+      Invitation.send_invitation(@reviewer, params[:invitation_message], task_path(:task_id => 1, :reviewer_id => @reviewer.id))
+      params[:invitation_email] = "Email sent to " + @reviewer.email 
+    end
+    #Sends an invitation to a reviewer with content (msg) and invitation link (url).
+  	ReviewerInviter.task_invitation(reviewer.email, msg, url).deliver()
+  	inv = Invitation.create(:status => STATUS[:pending])
+    inv.reviewer = reviewer
+    inv.save
+  end
 end
