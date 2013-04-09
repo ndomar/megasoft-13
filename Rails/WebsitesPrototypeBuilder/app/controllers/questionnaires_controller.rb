5c6ef9cbd250febd3b630fa581a1d5929513cb62
Ahmed Jihad Matar <aj.matar10@gmail.com>
2013-04-09 15:26:10 +0200

    merged with questionnaire branch

--- Rails/WebsitesPrototypeBuilder/app/controllers/questionnaires_controller.rb
+++ Rails/WebsitesPrototypeBuilder/app/controllers/questionnaires_controller.rb
@@ -31,7 +31,7 @@ class QuestionnairesController < ApplicationController
   def new
     @questionnaire = Questionnaire.new
       question = @questionnaire.qquestions.build
-      3.times {question.choices.build}
+      question.choices.build
     respond_to do |format|
       format.html # new.html.erb
       format.json { render json: @questionnaire }
