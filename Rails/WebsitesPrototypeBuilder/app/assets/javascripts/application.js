c26b0c46502e64e48eae243fda24c8d5b8a94abd
Maged Shalaby <maged918@gmail.com>
2013-04-09 12:55:00 +0200

    removed require tree to fix javascript error

--- Rails/WebsitesPrototypeBuilder/app/assets/javascripts/application.js
+++ Rails/WebsitesPrototypeBuilder/app/assets/javascripts/application.js
@@ -12,7 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require_tree .
 //= require bootstrap
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
