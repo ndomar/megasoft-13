5afd8368fc08c16ab3709cd1ed6e5d79c243f598
oss <oss@oss-HP-Mini-210-4000.(none)>
2013-04-09 18:01:32 +0200

    added method in model

--- Rails/WebsitesPrototypeBuilder/app/models/task.rb
+++ Rails/WebsitesPrototypeBuilder/app/models/task.rb
@@ -5,4 +5,12 @@ class Task<ActiveRecord::Base
   has_many :task_results
   has_and_belongs_to_many :reviewers
   validates :name, :presence => true
+  
+   def send_invitation(email, msg, url)
+    @reviewer = Reviewer.find_by_email(email)
+    if @reviewer == nil
+      @reviewer = this.reviewers.create(:email => email) 
+    end
+    ReviewerInviter.task_invitation(email, msg, url).deliver()
+  end
 end
