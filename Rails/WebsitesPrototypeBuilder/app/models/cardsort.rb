b42bd7d37d7246fd1047beb535bbc6be1f484e42
sshihata <samy.shihata@yahoo.com>
2013-04-09 16:32:41 +0200

    redoc

--- Rails/WebsitesPrototypeBuilder/app/models/cardsort.rb
+++ Rails/WebsitesPrototypeBuilder/app/models/cardsort.rb
@@ -1,11 +1,11 @@
 ##
 # represents a cardsort entity
-# **Attribute* :
+# * *Attribute* :
 # - +description+ -> a short desciption of the card sort and its purpose
 # - +open+ -> boolean to set the type of the cardsort
 # - +project_id+ -> the project the cardsort belongs to
 # - +title+ -> the name of the cardsort
-# **Relations* :
+# * *Relations* :
 # - belongs to project
 #
 class Cardsort < ActiveRecord::Base
