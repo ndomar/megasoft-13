1651c845b104df22515f70bb6dc361b08c10bb44
Ahmed Jihad Matar <aj.matar10@gmail.com>
2013-04-10 18:37:06 +0200

    controller methods

--- Rails/WebsitesPrototypeBuilder/app/controllers/answer_questionnaires_controller.rb
+++ Rails/WebsitesPrototypeBuilder/app/controllers/answer_questionnaires_controller.rb
@@ -1,5 +1,25 @@
 class AnswerQuestionnairesController < ApplicationController
-
-  def show
+ 	## 
+ 	# called to create a new question in the database
+  # finds the selected page
+  # * *Args* :
+  # - +@qquestion+ -> the id of the question he's answering
+  # - +@answer+ -> the answer the user wants to save
+  # * *Returns* :
+  # - page refreshed and question added to db
+  #
+  def create
+    # finds the selected page
+    @questionnaire = Questionnaire.find(params[:questionnaire_id])
+  	@qquestion = @questionnaire.qquestions.find(params[:qquestion_id])
+  	@answer =@qquestion.answers.build(params[:body], :presence => true)
+    respond_to do |format|
+    if @answer.save
+      # if Succedeed notify the user, and redirect to the reviewing page
+      format.html { redirect_to :controller => :questionnaire, :action => :show, :id => @questionnaire,:notice => 'Answer was successfully created.' }
+  	else
+      # if did not Succedeed notify the user, and refresh and give a notice
+      format.html {redirect_to :controller => :questionnaire, :action => :answer_show, :id => @questionnaire,:notice => 'Answer was not successfully created.' }
+    end
   end
 end
--- Rails/WebsitesPrototypeBuilder/db/migrate/20130409000722_create_answer_questionnaires.rb
+++ Rails/WebsitesPrototypeBuilder/db/migrate/20130409000722_create_answer_questionnaires.rb
@@ -3,13 +3,10 @@ class CreateAnswerQuestionnaires < ActiveRecord::Migration
     create_table :answer_questionnaires do |t|
       t.string :answer
       t.string :string
-      t.references :project
       t.references :questionnaire
       t.references :qquestion
-
       t.timestamps
     end
-    add_index :answer_questionnaires, :project_id
     add_index :answer_questionnaires, :questionnaire_id
     add_index :answer_questionnaires, :qquestion_id
   end
