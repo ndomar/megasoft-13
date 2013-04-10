e10e2a84a9dd8baade50f0ecebf343b332a4f61b
Maged Shalaby <maged918@gmail.com>
2013-04-10 23:25:49 +0200

    added documentation for model

--- Rails/WebsitesPrototypeBuilder/app/models/step.rb
+++ Rails/WebsitesPrototypeBuilder/app/models/step.rb
@@ -1,4 +1,16 @@
 class Step < ActiveRecord::Base
+  ##
+  # The steps belonging to a task.
+  # * *Attribute* :
+  # - +component+ -> string, saving the ID of the component associated with this step.
+  # - +description+ -> text, saving the description of what the reviewer should do in this step (can't be empty).
+  # - +event+ -> string, saves the event that should be done in this step.
+  # - +task_id+ -> integer, saves the id of the task for which this step belongs.
+  #* *Relations* :
+  # - has foreign key to the page and the reviewer
+  # - has many to one relation with model Task.
+  #
+
   belongs_to :task
   attr_accessible :component, :description, :event, :task_id
 
