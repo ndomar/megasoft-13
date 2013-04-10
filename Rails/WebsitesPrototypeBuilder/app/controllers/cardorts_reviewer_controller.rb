a5640329dd1bd4aab676451e168070ee8eecc3d4
Maya Ammar <mayaammar93@gmail.com>
2013-04-10 23:15:19 +0200

    view done

--- Rails/WebsitesPrototypeBuilder/app/controllers/cardorts_reviewer_controller.rb
+++ Rails/WebsitesPrototypeBuilder/app/controllers/cardorts_reviewer_controller.rb
@@ -2,5 +2,6 @@ class CardortsReviewerController < ApplicationController
   def groupsandcards
     @cardsort = Cardsort.find(params[:id])
     @cards = @cardsort.cards
+    @groups = @cardsort.groups
   end
 end
