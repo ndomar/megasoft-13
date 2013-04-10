478823bc27753997a6b356963c45ad1c73397f93
Sarah Nouh <sarah.nouh17@gmail.com>
2013-04-10 05:46:52 +0200

    storing number of clicks, starting time and total time in task_results

--- Rails/WebsitesPrototypeBuilder/app/controllers/steps_controller.rb
+++ Rails/WebsitesPrototypeBuilder/app/controllers/steps_controller.rb
@@ -1,13 +1,30 @@
 class StepsController < ApplicationController
   def update
-    if params[:change_id].to_s <=Step.last.id.to_s
-      @step=Step.find(params[:change_id])
+    @task =Task.find_by_id(1)
+    if params[:change_id].to_s <= @task.steps.last.id.to_s
+      @step=@task.steps.find(params[:change_id])
+      #@step_answer.step_id=@step.id
+      @pre_step = @task.steps.find(params[:change_id].to_f-1)
+      @step_answer=@pre_step.step_answers.new
+      @step_answer.successful= params[:change_success]
+      @step_answer.time_from_start= params[:start_time]
+      @step_answer.save
+
+      @task_result= @task.task_results.find(session[:task_result_id])
+      @task_result.click= params[:change_clicks]
+      @task_result.time_taken=params[:total_time_taken]
+      @task_result.save
     else
-      @step= Step.find_by_id(1)
+
        #Step.last(1).id <= params[:change_id]
     end
     respond_to do |format|
-      format.html { render :template => "tasks/task_reviewer" }
+      if params[:change_id].to_s <= @task.steps.last.id.to_s
+        format.html { render :template => "tasks/task_reviewer" }
+      else
+        format.html { render :template => "tasks/task_reviewer_done" }
+      end
+
     end  
   end
 end
--- Rails/WebsitesPrototypeBuilder/app/controllers/tasks_controller.rb
+++ Rails/WebsitesPrototypeBuilder/app/controllers/tasks_controller.rb
@@ -2,11 +2,13 @@ class TasksController < ApplicationController
 
   def task_reviewer
     @task= Task.find_by_id(1)
+    #@page= Page.find_by_id(1)
     @step=@task.steps.find_by_id(1)
     @step_answer=@step.step_answers.new
     @step_answer.save
     @task_result=@task.task_results.new
     @task_result.save
+    session[:task_result_id]= @task_result.id 
     #@task_res= 
   end
 end
