fa2bba3eebafdf1b8bdb347bcbdd23ccd661d939
Sarah Nouh <sarah.nouh17@gmail.com>
2013-04-10 21:12:35 +0200

    add the task page's html to the current page

--- Rails/WebsitesPrototypeBuilder/app/controllers/tasks_controller.rb
+++ Rails/WebsitesPrototypeBuilder/app/controllers/tasks_controller.rb
@@ -2,7 +2,7 @@ class TasksController < ApplicationController
 
   def task_reviewer
     @task= Task.find_by_id(1)
-    #@page= Page.find_by_id(1)
+    @page= Page.find_by_id(1)
     @step=@task.steps.find_by_id(1)
     @step_answer=@step.step_answers.new
     @step_answer.save
