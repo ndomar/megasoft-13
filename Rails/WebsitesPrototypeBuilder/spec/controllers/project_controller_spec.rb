d444845f361a61b5c34250400cc8531a81e07736
Menna Ashraf Essawy <mennaessawy12@gmail.com>
2013-04-11 21:56:38 +0200

    unit tests

--- Rails/WebsitesPrototypeBuilder/spec/controllers/project_controller_spec.rb
+++ Rails/WebsitesPrototypeBuilder/spec/controllers/project_controller_spec.rb
@@ -2,23 +2,14 @@ require 'spec_helper'
 
 describe ProjectsController do
 
-  #before(:each) do
-   # @attr = {
-    #  :project_name => "Test Project",
-     # :project_type => "Test type",
-      #:description => "Description",
-      #:designer_id => "1"
-   # }
-  #end
-
-  it "should create a new instance" do
-    get :new
-    assigns(:project).should_not be_nil
-  end
-
   it "should create a new instance given a valid attributes" do
-    get :create, {:project_name => 'mega', :designer_id => 1}
-    assigns(:project).should_not be_nil
+    count_before = Project.all.count
+    puts count_before
+    get :create, {:project => {:project_name => 'mega', :designer_id => '1'}}
+    count_after = Project.all.count
+    puts count_after
+    count_before.should be < count_after
+
   end
 
 end
