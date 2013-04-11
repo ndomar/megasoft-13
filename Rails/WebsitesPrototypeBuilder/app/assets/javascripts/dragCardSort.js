30432ef48a44135684cc3429310ef2ea1f47f867
george samy <georgesamy0@gmail.com>
2013-04-11 15:29:35 +0200

    added documentation

--- Rails/WebsitesPrototypeBuilder/app/assets/javascripts/dragCardSort.js
+++ Rails/WebsitesPrototypeBuilder/app/assets/javascripts/dragCardSort.js
@@ -1,3 +1,4 @@
+//When a card is dragged from one container to the other, it's "id" is removed from it's previous container and added to the new one
 $(document).ready(function(){
 	$('.container').sortable({containment:'document',tolerance:'pointer',cursor:'pointer', connectWith:'.container'});
 	$('.container').droppable({hoverClass:'border'});
