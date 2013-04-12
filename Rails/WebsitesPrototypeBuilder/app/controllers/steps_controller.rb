1932b0eb59b309203cf91598dde55447b76f786b
marwanelderwy <marwanelderwy@gmail.com>
2013-04-12 15:12:03 -0700

    useless

---
+++ Rails/WebsitesPrototypeBuilder/app/controllers/steps_controller.rb
@@ -0,0 +1,30 @@
+class StepsController < ApplicationController
+  ## 
+#updates the value of the current step and stores success,clicks,starting time and total time taken in their tables
+# * *Args*    :
+#   -+@task+ -> the current task
+#   -+@step+ -> the current step being executed
+#   -+@step_answer+ -> a new instance of step_answer contains the info of the current step
+#   -+@task_result+ -> a new instance of task_result contains the info about the current task's results
+# * *Returns*    :
+# - the current task, current step, step_answer for the current_task and task_result for the current task
+#
+  def update
+    @task =Task.find_by_id(params[:task_id])
+    @task.update_taskResults(params,session[:task_result_id])
+    returnHash = @task.update_taskResults(params,session[:task_result_id])
+    if params[:change_id].to_f <= @task.steps.last.id.to_f
+      @step = returnHash[:step]
+      @page = returnHash[:page]
+      @step_answer=returnHash[:step_answer]
+      @task_result=returnHash[:task_result]
+    end
+    respond_to do |format|
+      if params[:change_id].to_s <= @task.steps.last.id.to_s
+        format.html { render :template => "tasks/task_reviewer" }
+      else
+        format.html { render :template => "tasks/task_reviewer_done" }
+      end
+    end
+  end
+end
--- Rails/WebsitesPrototypeBuilder/spec/factories/comment.rb
+++
@@ -1,8 +0,0 @@
-FactoryGirl.define do
-  factory :comment do
-    body "My comment"
-    assigned_part "1"
-    page_id 1
-    reviewer 1
-  end
-end
