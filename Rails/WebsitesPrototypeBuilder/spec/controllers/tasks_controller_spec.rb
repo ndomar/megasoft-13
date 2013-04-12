d2af2956e7eeb49d419e50db61453bccc2c23d82
Maged Shalaby <maged918@gmail.com>
2013-04-12 15:52:54 +0200

    added delete step test

--- Rails/WebsitesPrototypeBuilder/spec/controllers/tasks_controller_spec.rb
+++ Rails/WebsitesPrototypeBuilder/spec/controllers/tasks_controller_spec.rb
@@ -11,6 +11,19 @@ describe TasksController do
     task = FactoryGirl.create(:task)
     get :new_step, id: task, event: "onclick", component: "test1", description: "test2"
     assigns(:created).should be_true
+    assigns(:step).task_id.should == task.id
+    assigns(:step).event.should  == "onclick"
+    assigns(:step).component.should == "test1"
+    assigns(:step).description.should == "test2"
+  end
+
+  it "Deletes a step" do
+    task = FactoryGirl.create(:task)
+    step = FactoryGirl.create(:step)
+    countBefore = task.steps.count
+    get :delete_step, task: task, id: step
+    countAfter = task.steps.count
+    countBefore.should be > countAfter
   end
 
 end
