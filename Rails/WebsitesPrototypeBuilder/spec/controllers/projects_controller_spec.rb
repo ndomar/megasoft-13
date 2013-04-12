e398a5958f4e0e4694ef365c8a48743b0ea0113f
Menna Ashraf Essawy <mennaessawy12@gmail.com>
2013-04-12 11:25:45 +0200

    unit tests.

--- Rails/WebsitesPrototypeBuilder/spec/controllers/projects_controller_spec.rb
+++ Rails/WebsitesPrototypeBuilder/spec/controllers/projects_controller_spec.rb
@@ -2,14 +2,24 @@ require 'spec_helper'
 
 describe ProjectsController do
 
+   before(:each) do
+    @attr = {
+      :project_name => 'Test Project',
+      :project_type => 'Test type',
+      :description => 'Description',
+      :designer_id => 2
+    }
+  end
+
   it "should create a new instance given a valid attributes" do
     count_before = Project.all.count
     puts count_before
-    get :create, {:project => {:project_name => 'mega', :designer_id => '1'}}
+    Project.create(@attr)
     count_after = Project.all.count
     puts count_after
     count_before.should be < count_after
 
   end
+
 
 end
