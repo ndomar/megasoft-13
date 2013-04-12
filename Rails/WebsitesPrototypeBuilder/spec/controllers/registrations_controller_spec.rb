e42d46e72e2ba98708fbc509e18da0748913364e
Ahmad Soliman <ahmadadel922gmail.com>
2013-04-12 18:05:32 +0200

    Code reviewed

--- Rails/WebsitesPrototypeBuilder/spec/controllers/registrations_controller_spec.rb
+++ Rails/WebsitesPrototypeBuilder/spec/controllers/registrations_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe RegistrationsController do
-    before(:each) do
+    before(:each)do
     @attr = {
       :fullname => "Test Designer",
       :email => "designer@gmail.com",
