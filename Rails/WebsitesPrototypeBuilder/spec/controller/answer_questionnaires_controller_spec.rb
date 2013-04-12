48f58827b6c0a2c90bbe66d9eb1d9fca4d7dda34
Ahmed Jihad Matar <aj.matar10@gmail.com>
2013-04-12 23:51:34 +0200

    changes in tests

--- Rails/WebsitesPrototypeBuilder/spec/controller/answer_questionnaires_controller_spec.rb
+++ Rails/WebsitesPrototypeBuilder/spec/controller/answer_questionnaires_controller_spec.rb
@@ -1,16 +1,19 @@
 require 'spec_helper'
 
-describe QuestionnairesController do
+describe AnswerQuestionnairesController do
 
   before(:each) do
    Questionnaire.create(:title=> "test questionnaire")
    Qquestion.create(:body => "question test",:number => 1,:qtype => 1, :questionnaire_id=>1)
   end
 
-  it "should create a new answer" do
-
-    post :create,{ :questionnaire_id => 1 , :qquestion_id => 1, :body => "test answer"}
-
-   	response.should redirect_to(:controller => :questionnaires, :action => :answer_show, :id => @questionnaire,:notice => 'Answer was successfully created.')
+  #Testing valid attributes as a params
+  context "with valid attributes" do
+    #Testing that the qustion is saved in the database
+    it "creates a new answer for question" do
+      expect{
+      post :create, :answer_questionnaire => {:body=> "test answer"}, :questionnaire_id => 1, :qquestion_id =>1
+      }.to change(AnswerQuestionnaire,:count).by(1)
+    end
   end
 end
