dc11cf6d5c0ca763537d0895cb9268562faf34c5
Menna Ashraf Essawy <mennaessawy12@gmail.com>
2013-04-12 12:40:57 +0200

    added some validations

--- Rails/WebsitesPrototypeBuilder/app/models/project.rb
+++ Rails/WebsitesPrototypeBuilder/app/models/project.rb
@@ -1,5 +1,6 @@
 # encoding: utf-8
 class Project < ActiveRecord::Base
+
   ##
   # Attributes:
   # This is the project model 
@@ -9,13 +10,14 @@ class Project < ActiveRecord::Base
   # * *Relations  :
   #   -A project belongs to one designer
   #   -A project has many tasks 
-  #   -A project has many pages
-  attr_accessible :description, :project_name, :project_type
+  attr_accessible :description, :project_name, :project_type, :designer_id
 
   validates :project_name, :presence => {:message => ".برجاء ادخال اسم المشروع"}
-  validates :project_name, :uniqueness => {:message => ".يوجد مشروع بهذا الاسم, برجاء ادخال اسم مشروع آخر"}
+  validates :project_name, :uniqueness => {:message => ".يوجد مشروع بهذا الاسم, برجاء ادخال اسم مشروع آخر",:scope => :designer_id}
 
-  belongs_to :designer
   has_many :tasks, :dependent => :destroy
   has_many :pages, :dependent => :destroy
+  belongs_to :designer
+
+
 end
