eb25e795d1f80956aa5d12b9ee5d4e3ac70a14cb
Maya Ammar <mayaammar93@gmail.com>
2013-04-12 20:33:29 +0200

    Reverted deleting CompanyProfiles

---
+++ CompanyProfiles/HeshamW/HeshamW.js
@@ -0,0 +1,17 @@
+function about_me()
+	{
+	document.getElementById("bio").innerHTML = "<h2> About Me </h2>" + 
+	"<p>I am interested in coding, history and literature</p>";
+	}
+
+function role()
+	{
+	document.getElementById("bio").innerHTML = "<h2> Company Role </h2>" + 
+	"<p> Employee </p>";
+	}
+
+function prog_tech()
+	{
+	document.getElementById("bio").innerHTML = "<h2> Programming Technology </h2>" + 
+	"<ul> <li>Java</li> <li>C#</li> <li>HTML</li> <li>SQL</li> <li>Prolog</li> </ul>";
+	}
