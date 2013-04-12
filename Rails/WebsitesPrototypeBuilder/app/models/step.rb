84af8c0d2efa54e2a272ba926ffbc3f9006095b6
Maged Shalaby <maged918@gmail.com>
2013-04-12 17:40:25 +0200

    fixed 4 spaces before colon documentation

--- Rails/WebsitesPrototypeBuilder/app/models/step.rb
+++ Rails/WebsitesPrototypeBuilder/app/models/step.rb
@@ -1,11 +1,11 @@
 ##
 # The steps belonging to a task.
-# * *Attribute* :
+# * *Attribute*    :
 # - +component+ -> string, saving the ID of the component associated with this step.
 # - +description+ -> text, saving the description of what the reviewer should do in this step (can't be empty).
 # - +event+ -> string, saves the event that should be done in this step.
 # - +task_id+ -> integer, saves the id of the task for which this step belongs.
-#* *Relations* :
+#* *Relations*    :
 # - has foreign key to the page and the reviewer
 # - has many to one relation with model Task.
 #
