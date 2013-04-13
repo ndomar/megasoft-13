0de7bb579dd3774a2d634e62d5c63175bb158156
marwanelderwy <marwanelderwy@gmail.com>
2013-04-13 16:19:20 -0700

    spec helper

--- Rails/WebsitesPrototypeBuilder/spec/mailers/send_notification_mail_spec.rb
+++ Rails/WebsitesPrototypeBuilder/spec/mailers/send_notification_mail_spec.rb
@@ -1,3 +1,4 @@
+require 'spec-helper'
 describe SendNotification do
   it "sends notification to designer" do
     send_notification("marwanelderwy@gmail.com", "asd", "derwy@_93@hotmail.com", "12:12")
