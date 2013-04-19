7d4a7f691f9881ed805156c9c1cdf5510cc7e03f
sshihata <samy.shihata@yahoo.com>
2013-04-19 14:47:34 +0200

    seeds

--- Rails/WebsitesPrototypeBuilder/app/models/page.rb
+++ Rails/WebsitesPrototypeBuilder/app/models/page.rb
@@ -5,7 +5,7 @@ class Page < ActiveRecord::Base
 	# - +html+ -> text, saving the html content of the designed page
 	# - +page_name+ -> string, saving the page name
 	#
-  attr_accessible :html ,:page_name, :project_id
+  attr_accessible :html ,:page_name, :project_id, :thumbnail
   # set it to contain many comments, when deleted delete all related comments
   has_many :comments,:dependent => :destroy
   # set it to contain many questions, when deleted delete all related questions
@@ -13,5 +13,4 @@ class Page < ActiveRecord::Base
   has_many :tasks
   has_many :answers,:dependent => :destroy
   belongs_to :project
-
 end
--- Rails/WebsitesPrototypeBuilder/app/models/questionnaire.rb
+++ Rails/WebsitesPrototypeBuilder/app/models/questionnaire.rb
@@ -13,7 +13,7 @@ class Questionnaire < ActiveRecord::Base
   belongs_to :project
   has_many :qquestions,:dependent => :destroy
   accepts_nested_attributes_for :qquestions, :reject_if => lambda {|a| a[:body].blank?}, :allow_destroy => true
-  attr_accessible :title
+  attr_accessible :title, :project_id
   attr_accessible :qquestions_attributes
   validates_presence_of :title
 	validates :title, :uniqueness => {:scope => :project_id}
