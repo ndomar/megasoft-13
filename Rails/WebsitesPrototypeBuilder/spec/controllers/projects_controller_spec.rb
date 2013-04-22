fa2175f61304382f025e2297b624a74fc5c317d7
Menna Ashraf Essawy <mennaessawy12@gmail.com>
2013-04-23 01:11:32 +0200

    unit tests.

---
+++ Rails/WebsitesPrototypeBuilder/spec/controllers/projects_controller_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+
+describe ProjectsController do
+  before(:each) do
+    @designer = FactoryGirl.create(:designer)
+    sign_in(@designer)
+  end
+
+  it"should create a new project" do
+    count_before  = Project.all.count
+    puts @count_before
+    post :create, project: {:project_name => 'holaa', :project_type => 'type', :description => 'hello', :designer_id => '1'}
+    count_after  = Project.all.count
+    puts @count_after
+    count_after.should be > count_before
+  end
+
+end
+
+
+describe ProjectsController do
+
+  before(:each) do
+    @designer = FactoryGirl.create(:designer)
+    sign_in(@designer)
+    @project = FactoryGirl.create(:project)
+  end
+
+  it "should delete a project" do
+    count_before  = Project.all.count
+    delete :destroy, :id => @project.id
+    count_after  = Project.all.count
+    count_after.should be < count_before
+  end
+
+end
