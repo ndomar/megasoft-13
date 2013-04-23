6c56057f6760249e4cd9a0e7a683bc2f85292065
Hesham ElWerdany <blackphoenix.008@gmail.com>
2013-04-23 15:15:10 +0200

    added doc for statistics_controller

--- Rails/WebsitesPrototypeBuilder/app/controllers/statistics_controller.rb
+++ Rails/WebsitesPrototypeBuilder/app/controllers/statistics_controller.rb
@@ -1,4 +1,11 @@
 class StatisticsController < ApplicationController
+  ## 
+  # passes the list of tasks that belongs to the project to the index view
+  # * *Args*    :
+  #   -+project_id+->: id of current project
+  # * *Returns* :
+  #   - list of tasks that belongs to project_id
+  #
   def index
     @tasks = Project.find(params[:project_id]).tasks
     @chosentasks = []
