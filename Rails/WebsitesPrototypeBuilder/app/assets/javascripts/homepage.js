9c8b7918e8d9c4365291c48883062a92025689e7
Ahmed Jihad Matar <aj.matar10@gmail.com>
2013-04-23 03:13:48 +0200

    more into design

--- Rails/WebsitesPrototypeBuilder/app/assets/javascripts/homepage.js
+++ Rails/WebsitesPrototypeBuilder/app/assets/javascripts/homepage.js
@@ -1,13 +1,18 @@
 // slide the login form
 $(document).ready(function sliding_form(){
   $('.login_image').click(function(){
-    if ($(".login_form").is(":hidden")){
-      $(".login_form").show("medium");
-    }
-    else{
-      $(".login_form").hide("fast");
-    }
-  });        
+		var offset = $(".login_image").offset();
+		var top_pos =offset.top;
+		var left_pos =offset.left;
+		$('.login_form').css({'top' : (top_pos)+'px', 'left' : (left_pos+40)+'px'});
+  	if ($(".login_form").is(":hidden")){
+    	$(".login_form").show("medium");
+  	}
+  	else{
+    	$(".login_form").hide("fast");
+  	}
+  }
+ );        
 });
 
 
