48b99f3305709dfcc0ec3e32f7a747d7eca839f9
George Samy <georgesamy0@gmail.com>
2013-04-23 05:27:27 -0700

    integrated password strength script

---
+++ Rails/WebsitesPrototypeBuilder/app/assets/javascripts/sign_up.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+	// $('#username').keyup(function(){$('#result').html(passwordStrength($('#password').val(),$('#username').val()))})
+	$('#password').keyup(function(){
+		$('#result').html(passwordStrength($('#password').val(),$('#email').val().split("@")[0]));
+		switch ($('#result').html()){
+			case "كلمة سر قصيرة":
+				$('#result').css("color","red");
+				$('#password').css("border-color", "red");
+				break;
+			case "ضعيف":
+				$('#result').css("color","red");
+				$('#password').css("border-color", "red");
+				break;
+			case "جيد":
+				$('#result').css("color","lime");
+				$('#password').css("border-color", "lime");
+				break;
+			case "قوي":
+				$('#result').css("color","green");
+				$('#password').css("border-color", "green");
+				break;
+		}
+	});
+
+});
