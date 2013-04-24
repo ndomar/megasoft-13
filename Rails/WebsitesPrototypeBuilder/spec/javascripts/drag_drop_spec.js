ce0ca5c5f5b4cd04caa28dfbef3e8d92fadba56d
Ahmed Jihad Matar <aj.matar10@gmail.com>
2013-04-25 00:51:44 +0200

    deleted word by mistake

--- Rails/WebsitesPrototypeBuilder/spec/javascripts/drag_drop_spec.js
+++ Rails/WebsitesPrototypeBuilder/spec/javascripts/drag_drop_spec.js
@@ -3,7 +3,7 @@ describe("DragDrop", function() {
     expect($('<div id="designpage">')).toExist();
   });
   it("contains spec with an expectation", function() {
-    expect($('<div id="toolbox">')).nottoExist();
+    expect($('<div id="toolbox">')).toExist();
   });
 });
 
