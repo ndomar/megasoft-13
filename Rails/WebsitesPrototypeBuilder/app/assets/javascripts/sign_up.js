0fc8934005f49f5b52a54bcea7f5f192afb9d52c
George Samy <georgesamy0@gmail.com>
2013-04-24 01:55:23 -0700

    added more user side validations to given information

--- Rails/WebsitesPrototypeBuilder/app/assets/javascripts/sign_up.js
+++ Rails/WebsitesPrototypeBuilder/app/assets/javascripts/sign_up.js
@@ -21,15 +21,6 @@ $(document).ready(function() {
 	// 			break;
 	// 	}
 	// });
-	$('input[type="submit"]').attr('disabled','disabled');
-	$('input[type="text"]').keyup(function(){
-		if($(this).val()!= ''){
-			$('input[type="submit"]').removeAttr('disabled');
-		}else {
-			$('input[type="submit"]').attr('disabled','disabled');
-		}
-	});
-
 	$('#password').keyup(function(){
 		$('#result').html(passwordStrength($('#password').val(),$('#email').val().split("@")[0]));
 		switch ($('#result').html()){
@@ -73,4 +64,43 @@ $(document).ready(function() {
 				break;
 		}
 	});
+	$('input[type="submit"]').attr('disabled','disabled');
+	$('input[type="text"]').keyup(function(){
+		if($(this).val()!= ''
+		    && document.getElementById("email").value!='' 
+			&& document.getElementById("box1").value!=''
+			&& document.getElementById("box2").value!=''
+			&& document.getElementById("box3").value!=''
+			&& document.getElementById("box4").value!=''
+			&& document.getElementById("box5").value!=''
+			&& (document.getElementById("box5").value<2012 && document.getElementById("box5").value>1900)
+			&& (document.getElementById("box4").value<13 && document.getElementById("box4").value>0)
+			&& (document.getElementById("box3").value<32 && document.getElementById("box3").value>0)
+			){
+			$('input[type="submit"]').removeAttr('disabled');
+		}else {
+			$('input[type="submit"]').attr('disabled','disabled');
+		}
+	});
+	$(document.getElementById("email")).keyup(function(){
+		if($(this).val()!= ''
+		    && document.getElementById("email").value!='' 
+			&& document.getElementById("box1").value!=''
+			&& document.getElementById("box2").value!=''
+			&& document.getElementById("box3").value!=''
+			&& document.getElementById("box4").value!=''
+			&& document.getElementById("box5").value!=''){
+			$('input[type="submit"]').removeAttr('disabled');
+		}else {
+			$('input[type="submit"]').attr('disabled','disabled');
+		}
+	});
+	$(document.getElementById("box5").keyup(function(){
+		if($(this).value>1900 || $(this).value<2012){
+			$('input[type="submit"]').removeAttr('disabled');
+		}else {
+			$('input[type="submit"]').attr('disabled','disabled');
+		}
+	}));
+	
 });
