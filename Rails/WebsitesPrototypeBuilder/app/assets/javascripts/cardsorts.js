43fd5683c1d319e3198a42ac3eb366b2d217f0a0
sshihata <samy.shihata@yahoo.com>
2013-04-28 12:48:57 +0200

    adjusting for chrome

--- Rails/WebsitesPrototypeBuilder/app/assets/javascripts/cardsorts.js
+++ Rails/WebsitesPrototypeBuilder/app/assets/javascripts/cardsorts.js
@@ -3,28 +3,36 @@ var groupsView;
 var cardsThumbnails;
 
 function init() {
-	cardsThumbnails = $('#cards-thumbnail');
-	cardsView = $('#cards-view').draggable({
+	setParams();
+	$('#cards-view').draggable({
 		handle: '#cards-tag',
 		axis: 'x',
 		drag: function() {
-				var difference = parseInt($(groupsView).css('right')) - parseInt($(cardsView).css('right'));
+				var difference = cardsView.offsetLeft - groupsView.offsetLeft;
 				var left = parseInt($(cardsView).css('left'));
 				if (difference < 24)
-					$(groupsView).css('left', left - 23);
-				width = parseInt($(cardsView).css('width')) + parseInt($(cardsView).css('right'));
-				$(cardsThumbnails).css('width', width+35);
+					$(groupsView).css('left', left - 24);
+				width = wrapper.offsetWidth - cardsView.offsetLeft;
+				$(cardsThumbnails).css('width', width);
 			}
 
 	});
-	groupsView = $('#groups-view').draggable({
+	$('#groups-view').draggable({
 		handle: '#group-tag',
 		axis: 'x',
 		drag: function() {
-				var difference = parseInt($(groupsView).css('right')) - parseInt($(cardsView).css('right'));
+				var difference =cardsView.offsetLeft - groupsView.offsetLeft;
 				var left = parseInt($(groupsView).css('left'));
 				if (difference < 24)
 					$(cardsView).css('left', left + 23);
 			}
 	});
+
+}
+
+function setParams() {
+	cardsView = $('#cards-view')[0];
+	groupsView = $('#groups-view')[0];
+	cardsThumbnails = $('#cards-thumbnail')[0];
+	wrapper = $('#wrapper')[0];
 }
