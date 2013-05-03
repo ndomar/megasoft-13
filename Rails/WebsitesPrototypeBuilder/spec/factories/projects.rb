408d821bc8d05e3b46f1aabee9829304769b7c32
Maged Shalaby <maged918@gmail.com>
2013-05-03 21:46:26 +0200

    added spec changes

--- Rails/WebsitesPrototypeBuilder/spec/factories/projects.rb
+++ Rails/WebsitesPrototypeBuilder/spec/factories/projects.rb
@@ -4,7 +4,5 @@ FactoryGirl.define do
     project_type 'project type'
     description 'Description'
     designer_id 1
-    # required if the Devise Confirmable module is used
-    # confirmed_at Time.now
   end
 end
